9c06fcc921cbdbc96ed57a408d3b51142dfc5ad6
e <2370484962@qq.com>
2020-07-30 20:13:44 +0800

    优化

--- src/views/transport/transportList/TransportDetail.vue
+++ src/views/transport/transportList/TransportDetail.vue
@@ -1,58 +1,273 @@
 <template>
   <div :class="isPC ? 'TransportDetail' : 'TransportDetail-m'">
     <template>
-      <el-tabs
-        v-model="activeName"
-        @tab-click="handleClick"
+      <el-radio-group
+        v-model="tabVal"
+        class="radioGroup"
+        style="margin-bottom: 30px;"
       >
-        <el-tab-pane
-          label="基本信息"
-          name="first"
-        >
-          <el-card>
-            <div class="baseBox">
-              <self-form
-                class="base"
-                :list-query="detailQuery"
-                :form-item="formItem"
-                label-width="80px"
+        <el-radio-button label="1">
+          基本信息
+        </el-radio-button>
+        <el-radio-button label="2">
+          标书信息
+        </el-radio-button>
+      </el-radio-group>
+
+      <div
+        v-show="tabVal === '1'"
+        class="ownerDetail"
+      >
+        <div class="baseBox">
+          <self-form
+            class="base"
+            :list-query="detailQuery"
+            :form-item="formItem"
+            label-width="80px"
+          >
+            <template slot="detail">
+              <router-link
+                :to="{path: '/join/orderdetail',query: {id: detailQuery.orderId}}"
+                class="link"
               >
-                <template slot="detail">
-                  <router-link
-                    :to="{path: '/join/orderdetail',query: {id: detailQuery.orderId}}"
-                    class="link"
-                  >
-                    详情>>
-                  </router-link>
-                </template>
+                详情>>
+              </router-link>
+            </template>
 
-                <template v-slot:payTypeName="{row}">
-                  {{ row.payTypeName === '' ? row.orderPayRecordInfoVOList[0].payTypeName : row.payTypeName }}
-                </template>
-                <template v-slot:payCompleteTime="{row}">
-                  {{ row.payCompleteTime | parseTime('{y}-{m}-{d} {h}:{i}:{s}') }}
-                </template>
-              </self-form>
-
-              <base-info :item="baseObj" />
-              <base-info :item="otherObj" />
-            </div>
-          </el-card>
-        </el-tab-pane>
-        <el-tab-pane
-          label="标书信息"
-          name="second"
+            <template v-slot:payTypeName="{row}">
+              {{ row.payTypeName === '' ? row.orderPayRecordInfoVOList[0].payTypeName : row.payTypeName }}
+            </template>
+            <template v-slot:payCompleteTime="{row}">
+              {{ row.payCompleteTime | parseTime('{y}-{m}-{d} {h}:{i}:{s}') }}
+            </template>
+          </self-form>
+        </div>
+        <SectionContainer
+          title="基本信息"
+          :md="true"
         >
-          <el-card>
-            <div class="bidBox">
-              <bid-info
-                :lists="bidLists"
-                :form-item="bidForm"
+          <el-row>
+            <el-col :span="isPC ? 6 : 24">
+              <DetailItem
+                name="运力编号"
+                :value="baseObj.carrierId"
               />
-            </div>
-          </el-card>
-        </el-tab-pane>
-      </el-tabs>
+            </el-col>
+
+            <el-col :span="isPC ? 6 : 24">
+              <DetailItem
+                name="姓名"
+                :value="baseObj.name"
+              />
+            </el-col>
+
+            <el-col :span="isPC ? 6 : 24">
+              <DetailItem
+                name="联系方式"
+                :value="baseObj.phone"
+              />
+            </el-col>
+
+            <el-col :span="isPC ? 6 : 24">
+              <DetailItem
+                name="业务线"
+                :value="baseObj.busiTypeName"
+              />
+            </el-col>
+
+            <el-col :span="isPC ? 6 : 24">
+              <DetailItem
+                name="工作城市"
+                :value="baseObj.workCityName"
+              />
+            </el-col>
+
+            <el-col :span="isPC ? 6 : 24">
+              <DetailItem
+                name="意向车型"
+                :value="baseObj.carTypeName"
+              />
+            </el-col>
+
+            <el-col :span="isPC ? 6 : 24">
+              <DetailItem
+                name="车牌号"
+                :value="baseObj.plateNo"
+              />
+            </el-col>
+
+            <el-col :span="isPC ? 6 : 24">
+              <DetailItem
+                name="运力状态"
+                :value="baseObj.statusName | DataIsNull"
+              />
+            </el-col>
+
+            <el-col :span="isPC ? 6 : 24">
+              <DetailItem
+                name="所属司机编号"
+                value=""
+              >
+                <router-link
+                  class="linkTo"
+                  :to="{path: '/join/orderdetail',query: {id: baseObj.driverId}}"
+                >
+                  {{ baseObj.driverId | DataIsNull }}
+                </router-link>
+              </DetailItem>
+            </el-col>
+
+            <el-col :span="isPC ? 6 : 24">
+              <DetailItem
+                name="所属司机"
+                :value="baseObj.driverName"
+              />
+            </el-col>
+
+            <el-col :span="isPC ? 6 : 24">
+              <DetailItem
+                name="所属司机手机号"
+                :value="baseObj.driverPhone"
+              />
+            </el-col>
+
+            <el-col :span="isPC ? 6 : 24">
+              <DetailItem
+                name="运营经理"
+                :value="baseObj.gmName"
+              />
+            </el-col>
+
+            <el-col :span="isPC ? 6 : 24">
+              <DetailItem
+                name="运营经理手机号"
+                :value="baseObj.gmPhone"
+              />
+            </el-col>
+
+            <el-col :span="isPC ? 6 : 24">
+              <DetailItem
+                name="创建人"
+                :value="baseObj.createName"
+              />
+            </el-col>
+
+            <el-col :span="isPC ? 6 : 24">
+              <DetailItem
+                name="创建人手机号"
+                :value="baseObj.createPhone"
+              />
+            </el-col>
+            <el-col :span="isPC ? 6 : 24">
+              <DetailItem
+                name="创建时间"
+                :value="baseObj.createDate"
+              />
+            </el-col>
+          </el-row>
+        </SectionContainer>
+        <SectionContainer
+          title="其他信息"
+          :md="true"
+        >
+          <el-row>
+            <el-col :span="isPC ? 6 : 24">
+              <DetailItem
+                name="司机年龄"
+                :value="otherObj.age| DataIsNull"
+              />
+            </el-col>
+
+            <el-col :span="isPC ? 6 : 24">
+              <DetailItem
+                name="户口类型姓名"
+                :value="otherObj.householdTypeName| DataIsNull"
+              />
+            </el-col>
+
+            <el-col :span="isPC ? 6 : 24">
+              <DetailItem
+                name="货物运输经验（月）"
+                :value="otherObj.workExperience| DataIsNull"
+              />
+            </el-col>
+
+            <el-col :span="isPC ? 6 : 24">
+              <DetailItem
+                name="家庭住址"
+                :value="otherObj.addr| DataIsNull"
+              />
+            </el-col>
+
+            <el-col :span="isPC ? 6 : 24">
+              <DetailItem
+                name="配送货物类型"
+                :value="otherObj.cargoTypeName| DataIsNull"
+              />
+            </el-col>
+
+            <el-col :span="isPC ? 6 : 24">
+              <DetailItem
+                name="期望月收入"
+                :value="otherObj.expMonthlyIncomeName| DataIsNull"
+              />
+            </el-col>
+
+            <el-col :span="isPC ? 6 : 24">
+              <DetailItem
+                name="平均月收入"
+                :value="otherObj.avgMonthlyIncome | DataIsNull"
+              />
+            </el-col>
+
+            <el-col :span="isPC ? 6 : 24">
+              <DetailItem
+                name="是否存在还贷款"
+                :value="otherObj.isIndebtedName | DataIsNull"
+              />
+            </el-col>
+
+            <el-col :span="isPC ? 6 : 24">
+              <DetailItem
+                name="所属司机"
+                :value="otherObj.driverName| DataIsNull"
+              />
+            </el-col>
+
+            <el-col :span="isPC ? 6 : 24">
+              <DetailItem
+                name="可接受一天工作时长"
+                :value="otherObj.maxWorkTimeName| DataIsNull"
+              />
+            </el-col>
+
+            <el-col :span="isPC ? 6 : 24">
+              <DetailItem
+                name="运营经理"
+                :value="otherObj.gmName| DataIsNull"
+              />
+            </el-col>
+
+            <el-col :span="isPC ? 6 : 24">
+              <DetailItem
+                name="备注"
+                :value="otherObj.remarks| DataIsNull"
+              />
+            </el-col>
+          </el-row>
+        </SectionContainer>
+      </div>
+      <div
+        v-show="tabVal === '2'"
+        class="clueDetail"
+      >
+        <div class="bidBox">
+          <bid-info
+            :lists="bidLists"
+            :form-item="bidForm"
+          />
+        </div>
+      </div>
     </template>
   </div>
 </template>
@@ -85,6 +300,7 @@ interface IState {
 
 export default class extends Vue {
   private activeName:string = 'first'
+  private tabVal: any = '1'
   private carrierId:string = ''
 
   private bidLists = [
@@ -274,45 +490,29 @@ export default class extends Vue {
 }
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 .TransportDetail {
-  width: 100%;
-  padding: 20px;
-  box-sizing: border-box;
-  .el-form-item__label{
-    color: #4a4a4a;
-    font-weight: 400;
-  }
-  .detail-group{
-    padding: 15px;
-  }
-  // .detail-title{
-
-  //   font-size: 14px;
-  //   color: #666;
-  //   font-weight: 400;
-  //   text-align: right;
-  //   padding-right: 16px;
-  //   box-sizing: border-box;
-  // }
-    .steps{
-      margin: 60px 0 30px 0;
-      }
-    .detail-title {
-      font-size: 13px;
-      color: #9e9e9e;
-      font-weight: 400;
-      padding-right: 16px;
-      -webkit-box-sizing: border-box;
-      box-sizing: border-box;
-      padding-bottom: 6px;
-    }
+    width: 100%;
+    padding: 20px;
+    background-color: white;
+    box-sizing: border-box;
 }
 </style>
 
 <style lang="scss" scoped>
 .TransportDetail-m {
   width: 100%;
+  background-color: white;
+    .link {
+        color: #649CEE;
+        font-weight:bold;
+      }
+      .radioGroup{
+          width: 100vw;
+          text-align: center;
+          margin-top: 20px;
+          margin-bottom: 0px!important;
+      }
 }
 </style>
 <style lang="scss" scoped>
@@ -321,27 +521,11 @@ export default class extends Vue {
         color: #649CEE;
         font-weight:bold;
       }
-  .bidBox,.baseBox{
-    min-height: calc(100vh - 250px);
-  }
 }
 </style>
-<style scoped>
-@media screen and (min-width: 701px) {
-  .el-select {
-    width: 100%;
-  }
-  .el-input{
-    width: 75%;
-  }
-}
-
-@media screen and (max-width: 700px) {
-  .el-select {
-    width: 100%;
-  }
-  .el-input{
-    width: 90%;
-  }
+<style scope>
+.linkTo{
+  color: #649CEE;
+  font-weight: bold;
 }
 </style>
