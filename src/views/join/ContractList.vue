2001b8f65063c2233776d9dd08fbe95baedc3515
zhaowei <zhaowei@yunniao.me>
2020-08-12 14:30:00 +0800

    修改激活按钮判断条件

--- src/views/join/ContractList.vue
+++ src/views/join/ContractList.vue
@@ -249,7 +249,7 @@
                     下载
                   </el-dropdown-item>
                   <el-dropdown-item
-                    v-if="scope.row.status === 2 || scope.row.status === 4"
+                    v-if="scope.row.status === 3"
                     v-permission="['/v1/order/contract/activeContract']"
                     name="contractlist_active_dropdown"
                     @click.native="Activate(scope.row.contractId)"
