2c8ce91b5ed4eb90c68188f0c2b2d111c79467a7
e <2370484962@qq.com>
2020-09-18 13:15:20 +0800

    api

--- src/api/system.ts
+++ src/api/system.ts
@@ -46,7 +46,11 @@ export const userDetail = (id: number) =>
     }
   })
 //
-
+export const getOfficeByCurrentUser = () =>
+  request({
+    url: `${baseURL}/v1/base/office/list/getOfficeByCurrentUser`,
+    method: 'get'
+  })
 // 组织管理-组织列表
 export const getOfficeList2 = (params: any) =>
   request({
@@ -117,11 +121,6 @@ export const createRole = (data: any = {}) =>
     method: 'post',
     data
   })
-export const getOfficeByCurrentUser = () =>
-  request({
-    url: `${baseURL}/v1/base/office/list/getOfficeByCurrentUser`,
-    method: 'get'
-  })
 // 角色管理-角色修改
 export const updateRole = (data: any = {}) =>
   request({
@@ -176,62 +175,4 @@ export const deleteAuthority = (id: number) =>
     params: {
       authId: id
     }
-  })
-
-// 获取用户列表-new
-export const getUserManagerList = (params:any) =>
-  request({
-    url: `${baseURL}/v1/base/user/page/list`,
-    method: 'get',
-    params
-  })
-// 获取组织架构-new
-export const getOfficeByCurrentUserV2 = () =>
-  request({
-    url: `${baseURL}/v2/base/office/list`,
-    method: 'get'
-  })
-
-export interface RoleParams {
-    dutyId?:number;
-    officeLevel:number;
-  }
-// 根据组织架构获取角色-new
-export const getDutyAndRoleList = (data:RoleParams) =>
-  request({
-    url: `${baseURL}/v2/base/duty/getDutyAndRoleList`,
-    method: 'post',
-    data
-  })
-
-// 创建用户-new
-export const addUser = (data:any) =>
-  request({
-    url: `${baseURL}/v2/base/user/create`,
-    method: 'post',
-    data
-  })
-
-// 修改用户-new
-export const modifyUser = (data:any) =>
-  request({
-    url: `${baseURL}/v2/base/user/update`,
-    method: 'post',
-    data
-  })
-
-// 启用或禁用用户
-export const enableOrDisableUser = (data:any) =>
-  request({
-    url: `${baseURL}/v2/base/user/enableOrDisable`,
-    method: 'post',
-    data
-  })
-
-// 同步用户到crm
-export const pushUserToCRM = (params:any) =>
-  request({
-    url: `${baseURL}/v2/base/user/pushUserToCRM`,
-    method: 'post',
-    params
   })
