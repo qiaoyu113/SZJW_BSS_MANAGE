8e3d5d339ef1a004f8f13171e518541c06c1db60
zhaowei <zhaowei@yunniao.me>
2020-09-23 17:31:57 +0800

    新建司机

---
+++ src/router/modules/driver-freight.ts
@@ -0,0 +1,28 @@
+import { RouteConfig } from 'vue-router'
+import Layout from '@/layout/index.vue'
+// 司机运费账户
+const driverAccountRouter: RouteConfig = {
+  path: '/driveraccount',
+  component: Layout,
+  redirect: '/driveraccount/list',
+  name: 'DriverAccount',
+  meta: {
+    title: 'driverAccount',
+    icon: 'driver',
+    apiUrl: 'root'
+  },
+  children: [
+    {
+      path: 'list',
+      component: () => import(/* webpackChunkName: "driver" */ '@/views/driver/DriverList.vue'),
+      name: 'DriverList',
+      meta: {
+        apiUrl: '/bss/v1/bss/driver/selectListByKey',
+        title: 'driverList',
+        noCache: false
+      }
+    }
+  ]
+}
+
+export default driverAccountRouter
