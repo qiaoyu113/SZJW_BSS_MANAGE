285dc18cd91fd08a32a87afc10557c2a7c4748aa
qiaoyu <qiaoyu@yunniao.me>
2020-09-23 16:33:27 +0800

    乔宇提交

---
+++ src/api/driver-account.ts
@@ -0,0 +1,13 @@
+import request from '@/utils/request'
+
+let prefix = '/driver'
+
+/**
+ * XXXX
+ */
+export const CreateActivity = (data: any) =>
+  request({
+    url: `${prefix}/v1/driver/clue/create/manually`,
+    method: 'post',
+    data
+  })
