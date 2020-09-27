2fab77bb747d2ea256b5b2f7e0d3ec9e9f6a2218
qiaoyu <qiaoyu@yunniao.me>
2020-09-27 15:40:15 +0800

    乔宇样式修改提交

--- src/router/modules/driver-account.ts
+++ src/router/modules/driver-account.ts
@@ -4,7 +4,7 @@ import Layout from '@/layout/index.vue'
 const driverAccountRouter: RouteConfig = {
   path: '/driveraccount',
   component: Layout,
-  redirect: '/driveraccount/list',
+  redirect: '/driveraccount/refundlist',
   name: 'DriverAccount',
   meta: {
     title: 'driverAccount',
@@ -20,6 +20,53 @@ const driverAccountRouter: RouteConfig = {
         apiUrl: '/bss/v1/bss/driver/selectListByKey',
         title: 'driverList',
         noCache: false
+      }
+    },
+    {
+      path: 'refundlist',
+      component: () => import(/* webpackChunkName: "driver" */ '@/views/driver-account/RefundList.vue'),
+      name: 'RefundList',
+      meta: {
+        apiUrl: 'root',
+        title: 'refundList',
+        noCache: false,
+        hidden: true
+      }
+    },
+    {
+      path: 'refunddetail',
+      component: () => import(/* webpackChunkName: "driver" */ '@/views/driver-account/RefundDetail.vue'),
+      name: 'RefundDetail',
+      redirect: '/driveraccount/refundlist',
+      meta: {
+        apiUrl: 'root',
+        title: 'refundDetail',
+        noCache: false,
+        hidden: true
+      }
+    },
+    {
+      path: 'refundaudit',
+      component: () => import(/* webpackChunkName: "driver" */ '@/views/driver-account/RefundAudit.vue'),
+      name: 'RefundAudit',
+      redirect: '/driveraccount/refundlist',
+      meta: {
+        apiUrl: 'root',
+        title: 'refundAudit',
+        noCache: false,
+        hidden: true
+      }
+    },
+    {
+      path: 'refundapply',
+      component: () => import(/* webpackChunkName: "driver" */ '@/views/driver-account/RefundApply.vue'),
+      name: 'RefundApply',
+      redirect: '/driveraccount/refundlist',
+      meta: {
+        apiUrl: 'root',
+        title: 'refundApply',
+        noCache: false,
+        hidden: true
       }
     }
   ]
