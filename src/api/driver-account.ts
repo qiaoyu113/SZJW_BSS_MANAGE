090605c3bc0a35472043cdef7c92a9c47e20b326
e <2370484962@qq.com>
2020-10-09 18:29:36 +0800

    司机梧桐账户

--- src/api/driver-account.ts
+++ src/api/driver-account.ts
@@ -1,6 +1,7 @@
 import request from '@/utils/request'
 
-let prefix = '/mock/112'
+const prefix = '/mock/112'
+const driverFix = '/business_center'
 
 /**
  * XXXX
@@ -44,3 +45,47 @@ export const accountUnfreeze = (data: any) =>
     method: 'post',
     data
   })
+
+/**
+   *
+   * 梧桐账户列表导出
+   */
+export const managementExport = (data: any) =>
+  request({
+    url: `${prefix}/v2/wt-driver-account/management/export`,
+    method: 'post',
+    data
+  })
+
+/**
+ * 订单列表
+ */
+export const orderList = (data: any) => {
+  return request({
+    url: `${driverFix}/v2/order/getOrderListByDriverId`,
+    method: 'get',
+    params: data
+  })
+}
+
+/**
+ * 订单详情
+ */
+export const orderDetail = (data: any) => {
+  return request({
+    url: `${driverFix}/v2/order/getOrderDetialByDriverId`,
+    method: 'get',
+    params: data
+  })
+}
+
+/**
+ * 订单详情
+ */
+export const countConfirmByDriver = (data: any) => {
+  return request({
+    url: `waybill/v2/waybill/shipping/countConfirmByDriver`,
+    method: 'get',
+    params: data
+  })
+}
