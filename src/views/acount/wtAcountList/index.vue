c38212e313eb0e92cedfa0ed36f7f6446c3116e6
yijie <839108892@qq.com>
2020-10-21 15:57:03 +0800

    1.修改搜集模糊搜索，清空从第一页搜索列表

--- src/views/acount/wtAcountList/index.vue
+++ src/views/acount/wtAcountList/index.vue
@@ -576,15 +576,15 @@ export default class extends Vue {
   ]
   private freezesRules:any = {
     orderId: [
-      { required: true, message: '请选择订单', trigger: 'blur' }
+      { required: true, message: '请选择订单', trigger: ['blur', 'change'] }
     ],
     applyForAccountFrozen: [
-      { required: true, message: '请填写申请冻结金额', trigger: 'blur' },
-      { validator: this.checkFrozen(1), trigger: 'blur' }
+      { required: true, message: '请填写申请冻结金额', trigger: ['blur', 'change'] },
+      { validator: this.checkFrozen(1), trigger: ['blur', 'change'] }
     ],
     applyForAccountUnfrozen: [
-      { required: true, message: '请填写申请解冻金额', trigger: 'blur' },
-      { validator: this.checkFrozen(2), trigger: 'blur' }
+      { required: true, message: '请填写申请解冻金额', trigger: ['blur', 'change'] },
+      { validator: this.checkFrozen(2), trigger: ['blur', 'change'] }
     ]
   }
   private isPass:Boolean = false
