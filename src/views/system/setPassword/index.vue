00726ead1b63e91854fa9c48a3dbe79e88daf893
e <2370484962@qq.com>
2020-11-18 11:46:27 +0800

    删除log

--- src/views/system/setPassword/index.vue
+++ src/views/system/setPassword/index.vue
@@ -192,7 +192,6 @@ export default class extends Vue {
     if (value === '') {
       callback(new Error('请再次输入密码'))
     } else if (value !== this.ruleForm.password1) {
-      console.log(value, this.ruleForm.password1)
       callback(new Error('两次输入密码不一致!'))
     } else {
       callback()
