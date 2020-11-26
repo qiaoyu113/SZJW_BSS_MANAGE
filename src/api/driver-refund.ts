3dacd0e606269ed0e8b7f235599655ba650d76e3
wangzhibo <coder_lufei@163.com>
2020-11-26 16:33:46 +0800

    新增列表功能

--- src/api/driver-refund.ts
+++ src/api/driver-refund.ts
@@ -2,7 +2,31 @@ import request from '@/utils/request'
 
 export const GetFreightChargeList = (data: any) =>
   request({
-    url: `$/v2/driverBilling/freightCharge/list`,
+    url: `/v2/driverBilling/freightCharge/list`,
     method: 'post',
     data
   })
+
+// /v2/wt-driver-account/refund/create
+// 创建申请退费
+export const createRefund = (data:any) => request({
+  url: `/v2/wt-driver-account/refund/create`,
+  method: 'post',
+  data
+})
+
+/// v2/wt-driver-account/refund/create/echo
+// 用户账户信息回显
+export const getRefundEcho = (params:any) => request({
+  url: `/v2/wt-driver-account/refund/create/echo`,
+  method: 'get',
+  params
+})
+// /v2/wt-driver-account/refund/haveRecordToBeApproved
+// 是否有待退款
+// http://szjw-domain-bill.m2.yunniao.cn/v2/wt-driver-account/refund/haveRecordToBeApproved?driverId=ewqweqeqe
+export const haveRecordToBeApproved = (params:any) => request({
+  url: `/v2/wt-driver-account/refund/haveRecordToBeApproved`,
+  method: 'get',
+  params
+})
--- src/views/driver-account/RefundApply.vue
+++ src/views/driver-account/RefundApply.vue
@@ -36,6 +36,8 @@ x<template>
               :remote-method="remoteMethod"
               :loading="driverLoading"
               placeholder="请选择"
+              @change="driverSelect"
+              @clear="handleClearQueryDriver"
             >
               <el-option
                 v-for="(item, index) in driverOtions"
@@ -84,9 +86,9 @@ x<template>
             </p>
           </template>
           <!--收据 -->
-          <template slot="receipt">
+          <template slot="hasReceipt">
             <el-radio-group
-              v-model="listQuery.receipt"
+              v-model="listQuery.hasReceipt"
               @change="delItem"
             >
               <el-radio :label="1">
@@ -97,8 +99,8 @@ x<template>
               </el-radio>
             </el-radio-group>
           </template>
-          <template slot="takeBackReceipt">
-            <el-radio-group v-model="listQuery.takeBackReceipt">
+          <template slot="recoveryReceipt">
+            <el-radio-group v-model="listQuery.recoveryReceipt">
               <el-radio :label="1">
                 有
               </el-radio>
@@ -134,6 +136,8 @@ x<template>
 import { Vue, Component, Watch } from 'vue-property-decorator'
 import SelfForm from '@/components/Base/SelfForm.vue'
 import { options } from 'numeral'
+import { getDriverNoAndNameList } from '@/api/driver'
+import { haveRecordToBeApproved, getRefundEcho, createRefund } from '@/api/driver-refund'
 interface IState {
   [key: string]: any
 }
@@ -148,8 +152,8 @@ interface listQuerys{
   hasReceipt:number // 是否有收据
   recoveryReceipt?:number // 是否回收收据
   driverId:string // 司机ID
-  reason:string
-  payMethod:number
+  reason:string // 退款原因
+  payMethod:number // 退款方式
   payeeName:string
 }
 @Component({
@@ -199,9 +203,9 @@ export default class extends Vue {
     return this.activeFrom
   }
   delItem(e: number) {
-    const inx = this.formItem.findIndex((item: any) => item.key === 'receipt')
+    const inx = this.formItem.findIndex((item: any) => item.key === 'hasReceipt')
     const inxs = this.formItem.findIndex(
-      (item: any) => item.key === 'takeBackReceipt'
+      (item: any) => item.key === 'recoveryReceipt'
     )
     let splicInx: number
     splicInx = inxs !== -1 ? 2 : 1
@@ -222,7 +226,7 @@ export default class extends Vue {
     },
     {
       type: 'driverId',
-      label: '司机姓名(司机编号):',
+      label: '司机姓名(司机编号/手机号):',
       key: 'driverId',
       slot: true
     },
@@ -330,31 +334,31 @@ export default class extends Vue {
     driverId: [
       { required: true, message: '选择是姓名+（手机号）', trigger: 'blur' }
     ],
-    refundAmount: [
+    money: [
       { required: true, message: '请输入申请退款金额!', trigger: 'blur' },
       { validator: this.validateMaxMoney, trigger: 'blur' }
     ],
-    reasonsRefund: [
+    reason: [
       { required: true, message: '请输入退款原因！', trigger: 'blur' }
     ],
-    refundmethod: [
+    payMethod: [
       { required: true, message: '请选择的退款方式！', trigger: 'blur' }
     ],
-    refundBankCardNumber: [
+    bankCardNo: [
       { required: true, message: '请输入退款银行卡号！', trigger: 'blur' },
       { pattern: /^[0-9]{0,30}$/, message: '银行卡号不可有特殊字符、空格，文字等', trigger: 'blur' }
     ],
-    bankDeposit: [
+    bankName: [
       { required: true, message: '请选择开户行！', trigger: 'blur' },
       { pattern: /^[a-zA-Z0-9\u4e00-\u9fa5]{1,50}$/, message: '银行卡号不可有特殊字符、空格，文字等', trigger: 'blur' }
     ],
-    receipt: [
+    hasReceipt: [
       { required: true, message: '请选择是否有收据！', trigger: 'change' }
     ],
-    takeBackReceipt: [
+    recoveryReceipt: [
       { required: true, message: '请选择收据是否收回', trigger: 'change' }
     ],
-    cardPensen: [
+    payeeName: [
       { required: true, message: '请输入持卡人姓名！', trigger: 'blur' },
       { max: 10, message: '持卡人姓名不可超过10个字', trigger: 'blur' }
     ]
@@ -395,11 +399,47 @@ export default class extends Vue {
     this.getDriverInfo(this.keyWord)
   }
   private driverOver: Boolean = false
-  private getDriverInfo(keyWord: string) {}
+  private async getDriverInfo(keyWord: string = '') {
+    try {
+      this.keyWord = keyWord
+      let params = {
+        key: ''
+      }
+      keyWord !== '' && (params.key = keyWord)
+      params = { ...params, ...this.driverPage }
+      if (this.driverOver) {
+        return
+      }
+      let { data: res } = await getDriverNoAndNameList(params, {
+        url: '/v2/wt-driver-account/flow/queryDriverList'
+      })
+      if (res.success) {
+        if (res.data.length && res.data.length > 0 && res.data.length === this.driverPage.limit) {
+          this.driverPage.page++
+        } else {
+          this.driverOver = true
+        }
+        let driverInfos = res.data.map(function(item: any) {
+          return {
+            label: `${item.name}/${item.phone}`,
+            value: item.driverId
+          }
+        })
+        this.driverOtions.push(...driverInfos)
+      } else {
+        this.$message.error(res.errorMsg)
+      }
+    } catch (err) {
+      console.log(err)
+    }
+  }
+  private handleClearQueryDriver() {
+    this.getDriverInfo()
+  }
   private async remoteMethod(query: any) {
     this.keyWord = query
     this.driverLoading = true
-    this.driverPage.page = 1
+    this.driverPage.page = 0
     this.driverOver = false
     this.driverOtions.splice(0, this.driverOtions.length)
     await this.getDriverInfo(this.keyWord)
@@ -425,7 +465,27 @@ export default class extends Vue {
     // this.listQuery.city = ''
     // this.listQuery.gmId = ''
   }
-  created() {}
+  async driverSelect(e:Event) {
+    console.log(e)
+    // 判断是否有已经退费的订单
+
+    try {
+      const { data } = await haveRecordToBeApproved({ driverId: e })
+      console.log(data)
+    } catch (error) {
+      return error
+    }
+    try {
+      const { data } = await getRefundEcho({ driverId: e })
+      console.log(data, 1)
+    } catch (error) {
+      return error
+    }
+    // 获取用户账户信息
+  }
+  created() {
+    this.getDriverInfo('')
+  }
 }
 </script>
 
