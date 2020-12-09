e0a4ff73f600c48ca25275ba098cb26c420e0518
yijie <839108892@qq.com>
2020-12-09 11:56:52 +0800

    1.司机线索添加mock  对字段(闫义杰)

--- src/views/driver-cloud/DriverClue.vue
+++ src/views/driver-cloud/DriverClue.vue
@@ -161,7 +161,7 @@ import SelfForm from '@/components/Base/SelfForm.vue'
 import SelfDialog from '@/components/SelfDialog/index.vue'
 import { GetDriverClueList, ExportDriverClue, allocationClue } from '@/api/driver-cloud'
 import { delayTime } from '@/settings'
-import { HandlePages, lock, parseTime } from '@/utils/index'
+import { HandlePages, lock, parseTime, showCityGroupPerson, showWork } from '@/utils/index'
 interface PageObj {
   page:number,
   limit:number,
@@ -301,7 +301,7 @@ export default class extends Vue {
         clearable: true,
         props: {
           lazy: true,
-          lazyLoad: this.showWork
+          lazyLoad: showWork
         }
       },
       label: '城市',
@@ -458,7 +458,7 @@ export default class extends Vue {
         clearable: true,
         props: {
           lazy: true,
-          lazyLoad: this.showCityGroupPerson
+          lazyLoad: showCityGroupPerson
         }
       },
       label: '选择跟进人',
@@ -477,58 +477,7 @@ export default class extends Vue {
   get isPC() {
     return SettingsModule.isPC
   }
-  // 获取客户城市
-  private async showWork(node:any, resolve:any) {
-    let query: any = {
-      parentId: ''
-    }
-    if (node.level === 1) {
-      query.parentId = node.value
-    }
-    try {
-      if (node.level === 0) {
-        let nodes = await this.areaAddress({ type: 2 })
-        resolve(nodes)
-      } else if (node.level === 1) {
-        let nodes = await this.cityDetail(query)
-        resolve(nodes)
-      }
-    } catch (err) {
-      resolve([])
-    }
-  }
-  // 获取大区列表
-  private async areaAddress(params: any) {
-    try {
-      let { data: res } = await getOfficeByType(params)
-      if (res.success) {
-        const nodes = res.data.map(function(item: any) {
-          return {
-            value: item.id,
-            label: item.name,
-            leaf: false
-          }
-        })
-        return nodes
-      }
-    } catch (err) {
-      console.log(`load city by code fail:${err}`)
-    }
-  }
-  // 根据大区获取城市列表
-  private async cityDetail(params: any) {
-    let { data: city } = await getOfficeByTypeAndOfficeId(params)
-    if (city.success) {
-      const nodes = city.data.map(function(item: any) {
-        return {
-          value: item.areaCode,
-          label: item.name,
-          leaf: true
-        }
-      })
-      return nodes
-    }
-  }
+
   // 获取业务线
   private async getDutyListByLevel() {
     try {
@@ -760,88 +709,7 @@ export default class extends Vue {
       console.log(`get base info fail:${err}`)
     }
   }
-  // 获取城市、小组、跟进人
-  async showCityGroupPerson(node: any, resolve: any) {
-    if (node.level === 0) {
-      let citys = await this.getOpenCitys()
-      resolve(citys)
-    } else if (node.level === 1) {
-      let groups = await this.getGroupInfoByCityCodeAndProductLine(+node.value)
-      resolve(groups)
-    } else if (node.level === 2) {
-      let [groupId, busiType] = node.value.split(',')
-      let users = await this.getGmOptions(node.parent.value, busiType, groupId)
-      resolve(users)
-    }
-  }
-  // 获取开通城市
-  async getOpenCitys() {
-    try {
-      let { data: res } = await GetOpenCityData()
-      if (res.success) {
-        return res.data.map((item:any) => ({
-          value: item.code,
-          label: item.name
-        }))
-      } else {
-        this.$message.error(res.errorMsg)
-      }
-    } catch (err) {
-      console.log(`get open city fail:${err}`)
-    } finally {
-      //
-    }
-  }
-  // 获取小组
-  async getGroupInfoByCityCodeAndProductLine(cityCode:number) {
-    try {
-      let params:IState = {
-        busiLine: [0, 1].toString(),
-        cityCode
-      }
-      let { data: res } = await getGroupInfoByCityCodeAndProductLine(params)
-      if (res.success) {
-        return res.data.map((item:any) => ({
-          value: item.id + ',' + item.dutyId,
-          label: item.name
-        }))
-      } else {
-        this.$message.error(res.errorMsg)
-      }
-    } catch (err) {
-      console.log(`get group fail:${err}`)
-    } finally {
-      //
-    }
-  }
-  // 获取小组下的人
-  async getGmOptions(cityCode:number, busiType:number, groupId:number) {
-    try {
-      let params:IState = {
-        roleTypes: [1],
-        cityCode,
-        busiType,
-        groupId,
-        uri: '/v2/driverBilling/shippingChange/queryGM'
-      }
 
-      let { data: res } = await GetSpecifiedRoleList(params)
-      if (res.success) {
-        return res.data.map(function(item: any) {
-          return {
-            label: item.name,
-            value: item.id,
-            disabled: item.status === 2,
-            leaf: true
-          }
-        })
-      } else {
-        this.$message.error(res.errorMsg)
-      }
-    } catch (err) {
-      console.log(err)
-    }
-  }
   mounted() {
     this.getLists()
     this.getDutyListByLevel()
