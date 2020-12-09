6f4fc576d33d9e10a987b5cfed782ea934aea77f
e <2370484962@qq.com>
2020-12-09 11:44:33 +0800

    跟进人状态更新（李威山）

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -503,9 +503,9 @@ async function getGmOptions(cityCode:number, busiType:number, groupId:number) {
     if (res.success) {
       return res.data.map(function(item: any) {
         return {
-          label: item.status === null ? item.name + `(停用)` : item.name,
+          label: item.status === 2 ? item.name + `(停用)` : item.name,
           value: item.id,
-          disabled: item.status === null,
+          disabled: item.status === 2,
           leaf: true
         }
       })
